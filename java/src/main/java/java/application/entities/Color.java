4f440af74fa4eac211b8ea0b34b822908b039938
Vadim Kazantsev <kazantsev97vs@gmail.com>
2020-03-08 10:07:25 +0700

    entity Color was added

---
+++ java/src/main/java/java/application/entities/Color.java
@@ -0,0 +1,25 @@
+package java.application.entities;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import javax.persistence.*;
+
+@Entity
+@Table(name = "color")
+@Data
+@NoArgsConstructor
+public class Color {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NonNull
+    @Column(unique = true)
+    private String name;
+
+    @NonNull
+    private String color;
+
+}
